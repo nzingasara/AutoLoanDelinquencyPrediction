a744937323af2caa7a2de3b8d90694bcffa67803
Nathan Florez <nathanflorez@lawn-128-61-35-176.lawn.gatech.edu>
2019-11-06 14:39:30 -0500

    updated code

---
+++ NeuralNetwork/neuralNetworkMortgageClassifier.py
@@ -0,0 +1,112 @@
+import numpy as np
+from keras.models import Sequential
+from keras.layers import *
+from keras.optimizers import Adam
+from keras.losses import categorical_crossentropy
+from keras.datasets import mnist
+import util
+import tensorflow as tf
+
+
+
+def create_model(input_shape, output_sizes):
+    model = Sequential()
+    model.add(Dense(output_size, ))
+    #for output_size in output_sizes:
+
+    return model
+
+def count_labels(df):
+    labels = df.values
+    count0 = 0
+    count1 = 0
+
+    for label in labels:
+        if label == 0:
+            count0 = count0 + 1
+        if label == 1:
+            count1 = count1 + 1
+
+    print("count0:")
+    print(count0)
+    print("count1:")
+    print(count1)
+
+# list of columns to hash encode
+cols_to_hash = ['state']
+no_new_cols_per = 6
+
+# load the data
+file_name = "mortgage_data_small.csv"
+df, unique_labels = util.load_data(file_name)
+
+print("df after loading (%ld rows):" % df.shape[0])
+print(df.head(n=5))
+
+# hash encode the data
+df = util.hash_encoder(df, cols_to_hash, no_new_cols_per)
+print("df after hashing (%ld rows):" % df.shape[0])
+print(df.head(n=5))
+
+# separate into train/test X/y splits
+df_train_X, df_train_y, df_test_X, df_test_y = util.get_train_test_split(df)
+
+print("df train X:")
+print(df_train_X.head(n=5))
+print('df train X rows:')
+print(df_train_X.shape[0])
+
+print("df train y head:")
+print(df_train_y.head(n=5))
+print("df test y head:")
+print(df_test_y.head(n=5))
+
+print("df_train_y label counts:")
+count_labels(df_train_y)
+print("df_test_y label counts:")
+count_labels(df_test_y)
+
+print("df test X:")
+print(df_test_X)
+print("df test X rows:")
+print(df_test_X.shape[0])
+
+print("df_train_X shape:")
+print(df_train_X.shape)
+print("df_test_X shape:")
+print(df_test_X.shape)
+
+# one hot encode the y data
+print("df_train_y.values type:")
+print(type(df_train_y.values))
+print("df_train_y.values[:10]:")
+print(df_train_y.values[:10])
+train_y = util.one_hot_encode(df_train_y.values, 2)
+test_y = util.one_hot_encode(df_test_y, 2)
+
+print("train_y one hot:")
+print(train_y[:10])
+
+model = Sequential()
+
+# get actual input shape and enter it here
+model.add(Dense(128, input_shape=(11,), activation='relu'))
+#model.add(LeakyReLU(0.1))
+model.add(Dense(64, activation='relu'))
+#model.add(LeakyReLU(0.1))
+model.add(Dense(32, activation='relu'))
+model.add(Dense(16, activation='relu'))
+model.add(Dense(2, activation='softmax'))
+
+# check if classes are relatively equal or not. If not, change metric
+model.compile(optimizer='adam', loss='categorical_crossentropy', metrics=['accuracy', tf.keras.metrics.Precision(), tf.keras.metrics.Recall()])
+
+# load in data and put in the x and y parts
+history = model.fit(x=df_train_X.values, y=train_y, epochs=5)
+print("history:")
+print(history)
+
+# put the test data in here
+results = model.evaluate(x=df_test_X.values, y=test_y)
+print("results evaluated:")
+print(results)
---
+++ NeuralNetwork/util.py
@@ -0,0 +1,173 @@
+import numpy as np
+import pandas as pd
+import category_encoders as ce
+from sklearn.model_selection import StratifiedShuffleSplit
+import math
+import datetime
+
+
+def get_data_stats(df):
+    # get all unique labels
+    unique_arr = df['delinquent'].unique()
+    print("unique labels:")
+    print(unique_arr)
+    print("unique labels count:")
+    print(len(unique_arr))
+
+    # count the numbr of each label UCR #
+    for label in unique_arr:
+        count = df[df['delinquent'] == label]['delinquent'].count()
+        print("%s: %ld" % (label, count))
+
+    return unique_arr
+
+
+def get_timestamp(date):
+    if "-" in date:
+        return datetime.datetime.strptime(date + "-01", "%Y-%m-%d").timestamp()
+    else:
+        return datetime.datetime.strptime(date + "01", "%Y%m%d").timestamp()
+
+
+# use Pred_Label
+def get_largest_cluster(df):
+    mode_series = df['Pred_Label'].mode()
+    return mode_series[0]
+
+
+def load_data(file_name):
+    type_dict = {'loan_amnt': np.float64,
+                 'state': 'str',
+                 'annual_inc': np.float64,
+                 'int_rate': np.float64,
+                 'delinquent': np.float64,
+                 'disbursal_date': 'str',
+                 'credit_score': np.float64
+                 }
+
+    df = pd.read_csv(file_name, header=0, delimiter=",", dtype=type_dict)
+
+    print("df shape before dropping Nans:")
+    print(df.shape[0])
+    # drop records that contain NaN
+    df.dropna(inplace=True)
+    print("df shape after dropping Nans:")
+    print(df.shape[0])
+
+    # create new column representing converted dates into timestamps
+    new_col = df["disbursal_date"].apply(get_timestamp)
+    df.insert(df.shape[1], "disbursal_timestamp", new_col, False)
+
+    # remove old disbursal_date column
+    df.pop('disbursal_date')
+
+    # shuffle stratified
+    df = df.sample(frac=1, replace=False)
+
+    # print the stats of the data
+    uniques = get_data_stats(df)
+
+    return df, uniques
+
+
+def get_train_test_split(df):
+    #largest_cluster = get_largest_cluster(df)
+
+    #df_train_X = df[df['Pred_Label'] == largest_cluster]
+    #df_test_X = df[df['Pred_Label'] != largest_cluster]
+
+    #####################################
+    # separate into train and test
+    num_rows = df.shape[0]
+    train_num_rows = math.floor(num_rows * 0.8)
+    df_train_X = df.iloc[:train_num_rows, :]
+    df_test_X = df.iloc[train_num_rows:, :]
+
+    # separate into two dataframes each, i.e. X, y
+    df_train_y = df_train_X.pop('delinquent')
+    df_test_y = df_test_X.pop('delinquent')
+    #######################################
+
+    # convert delinquent column to integer
+    print("df_train_y in get_train_test_split:")
+    print(df_train_y.head(n=5))
+    df_train_y = df_train_y.astype(np.int32)
+    df_test_y = df_test_y.astype(np.int32)
+
+    return df_train_X, df_train_y, df_test_X, df_test_y
+
+
+
+
+# modifies the dataframe by hashing the columns given into no_new_coils_per new columns, removes old cols
+# return: the transformed dataframe
+def hash_encoder(df, cols, no_new_cols_per):
+    print("<hash> df rows: %ld" % df.shape[0])
+    for col in cols:
+        print("hashing col %s" % col)
+        ce_hash = ce.HashingEncoder(cols=[col], n_components=no_new_cols_per)
+        X = df[col]
+        new_cols_df = ce_hash.fit_transform(X)
+        print("new cols df rows: %ld" % new_cols_df.shape[0])
+        df = df.drop(col, axis=1)
+        for i in range(0, no_new_cols_per):
+            placeholder_name = "col_%ld" % i
+            new_col_name = "%s%s%ld" % (col, "_", i)
+            #print("new_cols_df before rename:")
+            #print(new_cols_df.head(n=1))
+            new_cols_df = new_cols_df.rename(columns={placeholder_name: new_col_name})
+            #print("new_cols_df after rename:")
+            #print(new_cols_df.head(n=1))
+
+        # append the new columns to the dataframe
+        print("BEFORE concatting for col %s" % col)
+        print("<hash> df rows: %ld" % df.shape[0])
+        print("<hash> new cols rows: %ld" % new_cols_df.shape[0])
+        df.reset_index(drop=True, inplace=True)
+        new_cols_df.reset_index(drop=True, inplace=True)
+        df = pd.concat([df, new_cols_df], axis=1)
+        print("concatting for col %s" % col)
+        print("<hash> df rows: %ld" % df.shape[0])
+
+    return df
+
+
+def ordinal_enc(df, ordinal_mapping=None):
+    used_labels = {}
+    ordinal_labels = []
+
+    if ordinal_mapping is not None:
+        used_labels = ordinal_mapping
+
+    raw_labels = df.values
+
+    ordinal_index = 0
+
+    for label in raw_labels:
+        if label not in used_labels:
+            used_labels[label] = ordinal_index
+            ordinal_index = ordinal_index + 1
+        ordinal_labels.append(used_labels[label])
+
+    print("raw_labels:")
+    print(raw_labels[:15])
+    print("raw_labels length:")
+    print(len(raw_labels))
+    print("ordinal_labels:")
+    print(ordinal_labels[:15])
+    print("ordinal_labels length:")
+    print(len(ordinal_labels))
+
+    return np.asarray(ordinal_labels)
+
+
+def map_unique(unique_labels):
+    dic = {}
+    for i, label in enumerate(unique_labels):
+        dic[label] = i
+
+    return dic
+
+
+def one_hot_encode(values, num_classes):
+    return np.eye(num_classes)[values]
